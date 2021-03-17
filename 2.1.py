96bc6279aecf86b7c5a5c841b6f7c814749efae5
sergiolpfdz <79875888+sergiolpfdz@users.noreply.github.com>
2021-03-17 15:27:42 -0600

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,9 @@
+PI = 3.1416
+# Pedimos el radio
+r = float( input('Introduce radio del círculo: ') )
+# Calculamos el área y perímetro
+a = PI * r * r
+p = 2 * PI * r
+# Damos los resultados
+print('Área =', a)
+print('Perímetro =', p)
---
+++ ex_1_1.py
@@ -0,0 +1,11 @@
+# Ejercicio 1
+print('Pepito')
+print('Cumpleanos: 22 de enero')
+edad = 42
+print('Tengo', edad, 'anos')
+cantante = 'Suzanne Vega'
+comida = 'rúcula'
+ciudad = 'Barcelona'
+print('Me gusta la música de', cantante)
+print('Me gusta cenar', comida)
+print('Vivo en', ciudad)
